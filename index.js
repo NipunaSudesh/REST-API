c0a1f15461a3275ac2798db5ab4736265fcbeed3
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-22 18:14:16 +0530

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser"); 
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRouter = require('./Routes/user');
+const userRouter = require('./Routes/user.js');
 
 
 const app = express();
