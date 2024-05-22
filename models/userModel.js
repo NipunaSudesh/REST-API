9176807cb8981b9e642cda1a9c25f058297af362
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-22 14:46:40 +0530

    created  userSchema

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name :{
+        type :String,
+        required :true
+    },
+    email :{
+        type:String,
+        required:true,
+        unique:true,  
+        trim:true  
+    
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        trim:true,
+        required:true
+    }
+}
+);
