9adf4cc82fd8c11d1e4c86935f0dd63e31a51b09
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-22 10:32:43 +0530

    created index  file

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express=require("express");
 const bodyPaeser=require("body-parser");
+const cors=require("cors");
+
+const app=express();
+app.use(cors());
+
+const PORT =5000;
+app.use(bodyPaeser.json());
+
+app.listen(PORT,()=>{
+    console.log("server is up and running on port "+port)
+});
